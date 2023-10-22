034797d8eabdea61d50143f9e247c307360a39a4
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-10-22 18:35:06 +0530

    #include<stdio.h> int main(){     int a1,a2,a3,b1,b2,b3,c,d;     scanf("%d%d%d%d%d%d%d%d",&d,&c,&a1,&a2,&a3,&b1,&b2,&b3);     if(a1+a2+a3>=150 && b1+b2+b3>=150 && 2*d>c){         printf("YES");     }     else if((a1+a3+a3>=150 or b1+b2+b3>=150) and c<d){         printf("YES");     }     else{         printf("NO");     } }

---
+++ Rock_and_the_orders.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a1,a2,a3,b1,b2,b3,c,d;
+    scanf("%d%d%d%d%d%d%d%d",&d,&c,&a1,&a2,&a3,&b1,&b2,&b3);
+    if(a1+a2+a3>=150 && b1+b2+b3>=150 && 2*d>c){
+        printf("YES");
+    }
+    else if((a1+a3+a3>=150 or b1+b2+b3>=150) and c<d){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+}
