a282b587bda4622b0b22fd32cc231a81a99006eb
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-11-19 08:49:40 +0530

    #include<stdio.h> int main(){     int a,b,x,y,sum=0,i;     scanf("%d%d%d%d",&a,&b,&x,&y);     for(i=a;i<=b;i++){         if(i%x==0 && i%y!=0){             sum+=i;         }     }     printf("%d",sum); }

---
+++ Sum_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,x,y,sum=0,i;
+    scanf("%d%d%d%d",&a,&b,&x,&y);
+    for(i=a;i<=b;i++){
+        if(i%x==0 && i%y!=0){
+            sum+=i;
+        }
+    }
+    printf("%d",sum);
+}
