301112e3ec94556bab7887d4e648c3a38f44f12d
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-11-23 11:21:29 +0530

    #include<stdio.h> int main(){     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a==b & b==c){         printf("Equilateral triangle");     }     else if(a==b or b==c or c==a){         printf("Isosceles triangle");     }     else{         printf("Scalene triangle");     } }

---
+++ Triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){ 
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c); 
+    if(a==b & b==c){      
+        printf("Equilateral triangle"); 
+    }  
+    else if(a==b or b==c or c==a){ 
+        printf("Isosceles triangle"); 
+    }  
+    else{
+        printf("Scalene triangle");
+    }
+}
