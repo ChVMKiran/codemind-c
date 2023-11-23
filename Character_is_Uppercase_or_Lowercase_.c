d02aa9aa515879b95d201c6b5d1d365b63b9a050
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-11-23 10:46:26 +0530

    #include<stdio.h> int main(){     char ch;     scanf("%c", &ch);     if(ch >= 'A' && ch <= 'Z'){         printf("uppercase alphabet");     }     else if(ch >= 'a' && ch <= 'z'){         printf("lowercase alphabet");     }     else{         printf("not an alphabet");     } }

--- Character_is_Uppercase_or_Lowercase_.c
+++ Character_is_Uppercase_or_Lowercase_.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int main(){
-    char ch; 
+int main(){   
+    char ch;   
     scanf("%c", &ch);
-    if(ch >= 'A' && ch <= 'Z'){
-        printf("uppercase alphabet");
-    } 
-    else if(ch >= 'a' && ch <= 'z'){
-        printf("lowercase alphabet");
+    if(ch >= 'A' && ch <= 'Z'){      
+        printf("uppercase alphabet");   
+    }
+    else if(ch >= 'a' && ch <= 'z'){       
+        printf("lowercase alphabet");   
     }
     else{
         printf("not an alphabet");
