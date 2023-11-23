3053ad9c973f60d5dcd5019dfd1d911677b54a4c
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-11-23 10:10:16 +0530

    #include<stdio.h> int main(){     int w1,w2,w3,w4;     scanf("%d%d%d%d",&w1,&w2,&w3,&w4);     if(w1>=10&w2>=10&w3>=10&w4>=10){         printf("YES");     }     else{         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){   
+    int w1,w2,w3,w4;  
+    scanf("%d%d%d%d",&w1,&w2,&w3,&w4);
+    if(w1>=10&w2>=10&w3>=10&w4>=10){  
+        printf("YES");    
+    }    
+    else{   
+        printf("NO"); 
+    }
+}
