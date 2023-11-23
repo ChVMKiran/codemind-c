d4c9233807d310b34ce2cfd3914d447cb6cd6443
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-11-23 11:05:42 +0530

    #include<stdio.h> int main(){     int p,c,b,m,cs;     scanf("%d%d%d%d%d",&p,&c,&b,&m,&cs);     float percentage = (p+c+b+m+cs)/5;     if(percentage>=90){         printf("Grade A");     }     else if(percentage>=80){         printf("Grade B");     }     else if(percentage>=70){         printf("Grade C");     }     else if(percentage>=60){         printf("Grade D");     }     else if(percentage>=40){         printf("Grade E");     }     else{         printf("Grade F");     } }

---
+++ Grades.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){    
+    int p,c,b,m,cs; 
+    scanf("%d%d%d%d%d",&p,&c,&b,&m,&cs); 
+    float percentage = (p+c+b+m+cs)/5;   
+    if(percentage>=90){       
+        printf("Grade A");   
+    }  
+    else if(percentage>=80){      
+        printf("Grade B");   
+    }    
+    else if(percentage>=70){   
+        printf("Grade C");   
+    }  
+    else if(percentage>=60){    
+        printf("Grade D");    
+    } 
+    else if(percentage>=40){  
+        printf("Grade E");    
+    }    
+    else{  
+        printf("Grade F"); 
+    }
+}
