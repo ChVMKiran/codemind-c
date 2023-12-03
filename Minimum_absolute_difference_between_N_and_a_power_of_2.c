2e6aa2850e932ad2f3274b1564d6dc7639050250
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-12-03 14:05:54 +0530

    #include<stdio.h> int main(){     int n,i=2,j=2,l,h;     scanf("%d",&n);     while(i<n){         if(n<i){         	break; 		}         i*=2;     }     j=i/2;     if(i-n<n-j){         printf("%d",i-n);     }     else{         printf("%d",n-j);     } }

--- Minimum_absolute_difference_between_N_and_a_power_of_2.c
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.c
@@ -3,13 +3,16 @@ int main(){
     int n,i=2,j=2,l,h;
     scanf("%d",&n);
     while(i<n){
+        if(n<i){
+        	break;
+		}
         i*=2;
     }
     j=i/2;
-    if(n-j<i-n){
-        printf("%d",n-j);
+    if(i-n<n-j){
+        printf("%d",i-n);
     }
     else{
-        printf("%d",i-n);
+        printf("%d",n-j);
     }
 }
