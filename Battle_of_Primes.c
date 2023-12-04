edee91f65433d8ae4f12e77e5b9bf48f968f9df8
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-12-04 08:30:02 +0530

    #include<stdio.h> int main(){     int n1,n2,i,j,n3,c;     scanf("%d%d",&n1,&n2);     int a=n1+n2;     for(i=a+1;i<=10000;i++){         c=0;         for(j=2;j<i;j++){             if(i%j==0){                 c++;                 break;             }         }         if(c==0){             n3=i;             break;         }     }     n3=n3-a;     printf("%d",n3); }

---
+++ Battle_of_Primes.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n1,n2,i,j,n3,c;
+    scanf("%d%d",&n1,&n2);
+    int a=n1+n2;
+    for(i=a+1;i<=10000;i++){
+        c=0;
+        for(j=2;j<i;j++){
+            if(i%j==0){
+                c++;
+                break;
+            }
+        }
+        if(c==0){
+            n3=i;
+            break;
+        }
+    }
+    n3=n3-a;
+    printf("%d",n3);
+}
