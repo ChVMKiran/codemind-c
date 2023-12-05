4a92dc3b2f9eb37ba41bcc7a1205bf3654e1b45e
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-12-05 15:10:03 +0530

    #include<stdio.h> #include<math.h> int main(){     int a,b,c,d;     scanf("%d%d",&a,&b);     c=abs(a-b);     d=c/10;     if(c%10==0){         printf("%d",d);     }     else{         printf("%d",d+1);     } }

---
+++ Number_Problem.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a,b,c,d;
+    scanf("%d%d",&a,&b);
+    c=abs(a-b);
+    d=c/10;
+    if(c%10==0){
+        printf("%d",d);
+    }
+    else{
+        printf("%d",d+1);
+    }
+}
