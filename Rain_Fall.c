7f4231ac26964c220066194f2793c34b0cefb613
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-12-08 18:42:35 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x<3) printf("LIGHT");     else if(x>=3 && x<7) printf("MODERATE");     else if(x>=7) printf("HEAVY");     else printf("%d",x); }

--- Rain_Fall.c
+++ Rain_Fall.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
-int main(){    
-    int x;   
+int main()
+{
+    int x;
     scanf("%d",&x);
-    if(x<3){        
-        printf("LIGHT");    
-    }
-    else if(x>=3 & x<7){
-        printf("MODERATE");
-    } 
-    else{
-        printf("HEAVY");
-    }
+    if(x<3) printf("LIGHT");
+    else if(x>=3 && x<7) printf("MODERATE");
+    else if(x>=7) printf("HEAVY");
+    else printf("%d",x);
 }
