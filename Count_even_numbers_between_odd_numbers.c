5f5c389fc89abd5a65d625159067f5bad90cdf0d
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-12-21 10:35:12 +0530

    #include<stdio.h> int main(){     int n,c=0,i,j;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     for(i=0;i<n;i++){         if(a[i]%2!=0){             for(j=i+1;j<n;j++){                 if(a[j]%2!=0){                     break;                 }                 c++;             }         }     }     printf("%d",c); }

---
+++ Count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,c=0,i,j;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        if(a[i]%2!=0){
+            for(j=i+1;j<n;j++){
+                if(a[j]%2!=0){
+                    break;
+                }
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+}
