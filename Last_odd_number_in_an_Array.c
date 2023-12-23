f169036e3bd9f80637e9d4a087cd50aee054c0c5
ChVMKiran <147791703+ChVMKiran@users.noreply.github.com>
2023-12-23 09:55:49 +0530

    #include<stdio.h> int main(){     int n,i,j;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++){         scanf("%d",&a[i]);         if(a[i]%2!=0){             j=a[i];         }     }     printf("%d",j); }

---
+++ Last_odd_number_in_an_Array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(a[i]%2!=0){
+            j=a[i];
+        }
+    }
+    printf("%d",j);
+}
