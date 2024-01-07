017239539a7cc628c12d98332da03bed9fe603af
Ch V M Kiran <147791703+ChVMKiran@users.noreply.github.com>
2024-01-07 11:09:03 +0530

    #include<stdio.h> int main() {     int n,c=0,i,m=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         if(a[i]==1)         {             c++;         }         if(a[i]==0||i==n-1)         {             if(m<c)             {                 m=c;                 c=0;             }         }
        }
            printf("%d ",m);
    }

--- Max_Consecutive_Ones.c
+++ Max_Consecutive_Ones.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
-int  main(){
-    int b,n,i,c=0,m=0;
+int main()
+{
+    int n,c=0,i,m=0;
     scanf("%d",&n);
     int a[n];
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++)
+    {
         scanf("%d",&a[i]);
-        if(a[i]==1){
+        if(a[i]==1)
+        {
             c++;
         }
-        if(a[i]==0||i==n-1){
-            b=c;
-            c=0;
-            if(m<b){
-                m=b;
+        if(a[i]==0||i==n-1)
+        {
+            if(m<c)
+            {
+                m=c;
+                c=0;
             }
         }
+        
     }
-    printf("%d",m);
+        printf("%d ",m);
 }
